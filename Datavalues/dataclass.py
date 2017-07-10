56ebdd556e15d2c804e21c0c252c4fb9f4004b8f
Cyril Welschen <cj.welschen@gmail.com>
2017-07-10 22:02:56 +0200

    Basic dataclass write. Ropeproject folder and stuff from klen-python-mode vim package

---
+++ Datavalues/dataclass.py
@@ -0,0 +1,29 @@
+'__author__' == 'cyril'
+
+
+class Datastruct(object):
+    """
+    Class model data (value or cost)
+    """
+
+    def __init__(self, value, error=None, args=None, kwargs=None):
+        """
+        Initialise data struct.
+        :param value: cost or value (usually numerical)
+        :param error: corresponding error
+        :param args: list of additional values
+        :param kwargs: dictionary of additional information
+        """
+        self.val = value
+        self.err = error
+        self.args = args
+        self.kwargs = kwargs
+        self.var_name = kwargs.get('variable_name', None)
+
+    @property
+    def value_type(self):
+        return type(self.val)
+
+    @property
+    def error_type(self):
+        return type(self.err)
