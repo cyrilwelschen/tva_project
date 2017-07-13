204d0da6bc2e4bab16fee7de0aa37fa619508822
Cyril Welschen <cj.welschen@gmail.com>
2017-07-14 00:14:29 +0200

    Andvances in filesystem: read directories and excels

--- utils/filesystem.py
+++ utils/filesystem.py
@@ -9,41 +9,87 @@ from openpyxl.reader.excel import load_workbook, InvalidFileException
 DATAPATH = "/home/cyril/Desktop/data_bix/"
 
 
-def list_files(path=None, **kwargs):
+def list_files(path=None):
     """
-    List available datafiles. By default relative to DATAPATH.
-    :param path: relative path wrt DATAPATH
-    :param relative: set to False to use absolute paths
+    List files and directories of path.
+    :param path: path to directory for display
+    :return list of directories and files sorted alphabetically.
     """
-    path = analyse_path(path, **kwargs)
+    path = analysed_path(path)
     file_list = os.listdir(path)
     file_list.sort()
     return file_list
 
 
-def analyse_path(path, relative=True):
+def analysed_path(path):
+    """
+    Analyse if path is provided, and if so if its relative or not
+    :param path: absolute or relative path or None
+    :return usable absolute path
+    """
     if not path:
-        path = DATAPATH
-    elif relative:
-        path = DATAPATH+path
-    return path
+        return DATAPATH
+    elif path[:5] == "/home":
+        return path
+    else:
+        return DATAPATH+path
 
 
-def list_excels(path=None, convert=True, **kwargs):
-    all_files = list_files(path=path, **kwargs)
+def list_filetype(ending, path=None):
+    """
+    List files of specific type (i.e. ending) in directory.
+    :param ending: string of ending with variable length. E.g. 'xls'
+    :param path: absolute or relative path or None
+    :return list of files with that endng.
+    """
+    all_files = list_files(path)
     xls = []
     for fl in all_files:
-        if (fl[-3:] == "xls") and convert:
-            pwd = os.getcwd()
-            path = analyse_path(path, relative=False)
-            os.chdir(path)
-            os.system("libreoffice --convert-to xlsx "+fl+" --headless")
-            os.chdir(pwd)
-            print("libreoffice --convert-to xlsx "+fl+" --headless")
-            xls.append(fl[:-3]+"xlsx")
-        if "xlxs" in fl:
+        if ending == fl[-len(ending):]:
             xls.append(fl)
     return xls
+
+
+def list_xls(path=None):
+    """
+    List all excel files of old type.
+    :param path: absolute, relative or None
+    :return: list of xls files
+    """
+    return list_filetype("xls", path)
+
+
+def list_xlsx(path=None):
+    """
+    List all excel files of new type.
+    :param path: absolute, relative or None
+    :return: list of xlsx files
+    """
+    return list_filetype("xlsx", path)
+
+
+def list_all_excels(path=None, merge=True):
+    """
+    List(s) all excel files, old and new type.
+    :param path: absolute, relative or None
+    :param merge: whether to return old and new type excels in sparate list.
+    Defaulte is returning a single list.
+    :return: list or two lists of xls and xlsx files
+    """
+    if merge:
+        return list_xls(path) + list_xlsx(path)
+    else:
+        return list_xls(path),  list_xlsx(path)
+
+
+def convert_excels(path_to_xls):
+    os.system("libreoffice --convert-to xlsx "+path_to_xls+" --headless")
+    print("libreoffice --convert-to xlsx "+path_to_xls+" --headless")
+    pass
+
+
+def convert_all_excels(path):
+    pass
 
 
 def excels_by_regex(path):
