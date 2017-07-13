c67ff4db672648755bb7d9a40a818811fc2a4c5d
Cyril Welschen <cj.welschen@gmail.com>
2017-07-14 01:36:05 +0200

    filesystem updates

--- utils/filesystem.py
+++ utils/filesystem.py
@@ -2,9 +2,6 @@
 
 
 import os
-import xlrd
-from openpyxl.workbook import Workbook
-from openpyxl.reader.excel import load_workbook, InvalidFileException
 
 DATAPATH = "/home/cyril/Desktop/data_bix/"
 
@@ -82,54 +79,37 @@ def list_all_excels(path=None, merge=True):
         return list_xls(path),  list_xlsx(path)
 
 
-def convert_excels(path_to_xls):
+def strip_file_of_path(path_to_file):
+    """
+    Strip the file (last part) off of the absolute path.
+    :param path_to_file: absolute path to a file e.g /home/cyril/test.png
+    :return absolute path without file e.g. /home/cyril/
+    """
+    return path_to_file[:-len(os.path.basename(path_to_file))]
+
+
+def convert_xls_to_xlsx(path_to_xls):
+    from time import time as ti
+    print("converting {} to xlsx format...".format(path_to_xls))
+    t0 = ti()
     os.system("libreoffice --convert-to xlsx "+path_to_xls+" --headless")
-    print("libreoffice --convert-to xlsx "+path_to_xls+" --headless")
-    pass
+    t1 = ti()
+    print("done in {} sec.".format(round(t1-t0, 2)))
+    xlsx_exist = os.path.exists(path_to_xls[:-3]+"xlsx")
+    print(xlsx_exist)
+    # assert xlsx_exist, "conversion of file {} failed".format(path_to_xls)
 
 
-def convert_all_excels(path):
+def convert_all_xls_to_xlsx(path=None):
+    """
+    converts all xls files in a directory to xlsx files
+    :param path: abs or rel path or none.
+    """
+    basepath = analysed_path(path)
+    for xls in list_xls(path):
+        convert_xls_to_xlsx(basepath+"/"+xls)
     pass
 
 
 def excels_by_regex(path):
     pass
-
-
-def open_xls_as_xlsx(filename):
-    # first open using xlrd
-    book = xlrd.open_workbook(filename)
-    index = 0
-    nrows, ncols = 0, 0
-    while nrows * ncols == 0:
-        sheet = book.sheet_by_index(index)
-        nrows = sheet.nrows
-        ncols = sheet.ncols
-        index += 1
-    # prepare a xlsx sheet
-    book1 = Workbook()
-    sheet1 = book1.get_active_sheet()
-    for row in range(0, nrows):
-        for col in range(0, ncols):
-            sheet1.cell(row=row, column=col).value = sheet.cell_value(row, col)
-    return book1
-
-
-def convert_xls_to_xlsx(path_to_file, keep_original=True):
-    filename = path_to_file
-    # first open using xlrd
-    book = xlrd.open_workbook(filename)
-    index = 0
-    nrows, ncols = 0, 0
-    while nrows * ncols == 0:
-        sheet = book.sheet_by_index(index)
-        nrows = sheet.nrows
-        ncols = sheet.ncols
-        index += 1
-    # prepare a xlsx sheet
-    book1 = Workbook()
-    sheet1 = book1.get_active_sheet()
-    for row in range(0, nrows):
-        for col in range(0, ncols):
-            sheet1.cell(row=row, column=col).value = sheet.cell_value(row, col)
-    return book1
