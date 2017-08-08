cce07dfab9f9e0296e1e134332a2653f0f1fdac1
Cyril Welschen <cj.welschen@gmail.com>
2017-08-08 16:31:47 +0200

    FTTC Opex

--- models/zero_to_hero_oneplot.py
+++ models/zero_to_hero_oneplot.py
@@ -6,8 +6,11 @@ import numpy as np
 from model_helpers import make_array
 from model_helpers import monatsweise_zu_total as m2t
 import sys
-sys.path.append("../.")
+home = '/home/cyril/Dropbox/arbeit/swisscom/'\
+        + 'Projekt3_RLA/Programming_venv/Programming'
+sys.path += [home]
 from tva_project.excelinterface.database import search_query
+
 
 db_file = '/home/cyril/Desktop/slim_csv_2017-04-Alle_Daten.db'
 
@@ -28,12 +31,12 @@ class Slider(object):
         return self.s
 
 
-rez = "asdf"
+rez = "598051-199322"
 nutzungseinheiten = 1
 hori = 12*20
 h_ob_fan = 521.7 + 1557.7 + 344.45 + 22.02  # Fan
 h_as_fan = 253.13  # Fan
-eff_b_fan = 432
+eff_b_fan = 576*0.75
 h_rla_ne = 1.54  # NE
 
 sb_ob_mcan = 25.47 + 22.36 + 4.63 + 11.78  # mCan
@@ -44,9 +47,11 @@ b_rla_mcan = 1300*0.05 + 50  # mCan
 s_as_mcan = 75.03  # mCan
 s_rla_mcan = 1300*0.05 + 200  # mCan
 s_ob_mcan = 138  # mCan Strom
-eff_b_mcan = 32
+eff_b_mcan = 31
 
-data = {}
+c_ob_can = 100.22 + 44.0 + 349.99 + 1136.26
+c_as_can = 82.93
+eff_b_can = 560*0.4
 
 
 def horizon():
@@ -55,6 +60,42 @@ def horizon():
 
 def get_ne():
     return nutzungseinheiten
+
+
+def look_for():
+    return ['ReZ', 'NENutzungseinheitenTotal', 'WEWohneinheitenTotal',
+            'PenetrationVerträgeRESWlineHSIanWETotal',
+            'FTTHTotalproNE', 'FTTSBTotalproNE', 'KupferleitungslängezumCO']
+
+
+def query(li=look_for()):
+    query = ''
+    for s in li:
+        query += s+', '
+    return query[:-2]
+
+
+def search(rez):
+    re = search_query(db_file, "SELECT {} from RezTable WHERE \
+                      Rez LIKE '%{}%'".format(query(), rez))
+    if len(re) != 1:
+        print("Found multiple rez fitting the description: {}\n\n \
+              Taking first by default".format(re))
+    else:
+        print("REE:", re[0])
+        return re[0]
+
+
+def rez_dic(rez):
+    re = search(rez)
+    rez_dic = {}
+    for k, v in zip(look_for(), re):
+        rez_dic[k] = v
+    return rez_dic
+
+
+def re_name(rez_dic):
+    return rez_dic['ReZ'].split()[0]+' '+rez_dic['ReZ'].split()[1]
 
 
 """
@@ -68,6 +109,14 @@ def fan(ne=1):
 
 def h_opex(ne=1):
     return fan(ne) + ne * h_rla_ne
+
+
+def can(ne=1):
+    return (c_ob_can + c_as_can) / eff_b_can * ne
+
+
+def c_opex(ne=1):
+    return can(ne) + agg(ne)
 
 
 def mcan(ne=1, t='ftts'):
@@ -91,7 +140,7 @@ def b_opex(ne=1):
 
 
 def techs():
-    return ['h', 's', 'b']
+    return ['h', 's', 'b', 'c']
 
 
 """
@@ -107,10 +156,6 @@ def m_nb2i_ne(t, ne=1):
     return m2t(eval('k2a('+t+'_opex({}), 12)'.format(ne)) / ne)
 
 
-def to_data(name, val_array):
-    data[name] = val_array
-
-
 def rn(t):
     if t == 'h':
         return 'FTTH'
@@ -118,6 +163,8 @@ def rn(t):
         return 'FTTB'
     if t == 's':
         return 'FTTS'
+    if t == 'c':
+        return 'FTTC'
 
 
 class LineBuilder:
@@ -149,17 +196,21 @@ def p_label(n, chf):
     return "{}: {} CHF".format(n, round(chf, 2))
 
 
-def plot(sli):
-    plt.close()
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 6))
-    x = np.arange(horizon())
+def ax1_plot(ax1, sli, x):
     for t in techs():
         y = m_nb2i_ne(t, get_ne())
-        ax1.plot(x, y, label=p_label(rn(t),y[sli.v()]))
+        ax1.plot(x, y, label=p_label(rn(t), y[sli.v()]))
     ax1.set_title(p_tit(sli.v()))
     ax1.legend()
     vl = ax1.axvline(sli.v())
     LineBuilder(vl, ax1)
+
+
+def plot_master(sli):
+    plt.close()
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 6))
+    x = np.arange(horizon())
+    ax1_plot(ax1, sli, x)
     plt.show()
     return 0
 
@@ -169,4 +220,5 @@ Main
 """
 if __name__ == '__main__':
     print(search_query(db_file, "SELECT Rez from RezTable WHERE ID < 6"))
-    plot(Slider())
+    plot_master(Slider())
+    print(rez_dic(rez))
